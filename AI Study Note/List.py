39a319a5181603ac28e985ddaa2f961b98dea700
Ayuba Tahiru <ayubamonnietahiru@gmail.com>
2021-06-09 09:08:47 +0200

    list

--- AI Study Note/List.py
+++ AI Study Note/List.py
@@ -21,3 +21,38 @@ first_names = ["ayuba", "zsuzsanna"]
 first_names.insert(1, "tahiru")
 first_names.insert(2, "imri")
 print(first_names) # ['ayuba', 'tahiru', 'imri', 'zsuzsanna']
+
+# remove(x), removes the first item from the list whose values is equal to the "x". raise ValueError if no such item
+first_names.remove("ayuba")
+print(first_names) # ['tahiru', 'imri', 'zsuzsanna']
+
+# pop([i]), removes the item at the given position in the list, if no position is given, it removes and return the last item
+index_zero_pop = first_names.pop(0)
+print(index_zero_pop) # tahiru
+no_index_pop = first_names.pop()
+print(no_index_pop) # zsuzsanna
+
+# clear(), remove all item from the list. equivalent to del a[:]
+a.clear()
+print(a) # []
+
+del b[:]
+print(b) # []
+
+
+# index(x[,start[,end]]), return zero_base index in the list of the first item whose value is equal to x, Raise a ValueError if there is no such item
+b = ["w", "a", "b", "c", "d","d", 2, 9, 10, 10]
+indexed_value = b.index(2)
+print(indexed_value) # 6
+
+# count(x), returns the number of times x appears in a list
+count_value = b.count("d")
+print(count_value) # 2
+
+c = ["w", "a", "b", "c", "d","d", "z", "q", "l"]
+c.sort()
+print(c) # ['a', 'b', 'c', 'd', 'd', 'l', 'q', 'w', 'z']
+
+# reverse(), reverse the element of the list
+c.reverse()
+print(c) # ['z', 'w', 'q', 'l', 'd', 'd', 'c', 'b', 'a']
